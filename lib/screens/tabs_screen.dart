6a85d44a71a8292f9c6ec24d785eed7a5dc2814e
Yurii Yesipov <yesipov47@gmail.com>
2021-05-22 20:39:39 +0300

    Added settings screen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -18,12 +18,16 @@ class _TabsScreenState extends State<TabsScreen> {
     Text(
       'Chats',
     ),
+    Text(
+      'Settings',
+    ),
   ];
 
   Widget _getAppBarTitle(BuildContext ctx, int index) {
     final List<String> _appBarTitles = [
       L10n.of(ctx).home_screen_app_bar_text,
       L10n.of(ctx).chats_screen_app_bar_text,
+      L10n.of(ctx).settings_screen_app_bar_text,
     ];
 
     return Text(_appBarTitles.elementAt(index));
@@ -54,17 +58,22 @@ class _TabsScreenState extends State<TabsScreen> {
             label: L10n.of(context).tabs_screen_chats_tab_text,
             icon: Icon(Icons.chat),
           ),
+          BottomNavigationBarItem(
+            label: L10n.of(context).tabs_screen_settings_tab_text,
+            icon: Icon(Icons.settings),
+          ),
         ],
         currentIndex: _currentTabIndex,
         onTap: _onItemTapped,
       ),
-      floatingActionButton: _currentTabIndex == 0 ? FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .pushNamed(AddEditPetScreen.routeName);
-        },
-        child: Icon(Icons.add),
-      ) : null,
+      floatingActionButton: _currentTabIndex == 0
+          ? FloatingActionButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddEditPetScreen.routeName);
+              },
+              child: Icon(Icons.add),
+            )
+          : null,
     );
   }
 }
