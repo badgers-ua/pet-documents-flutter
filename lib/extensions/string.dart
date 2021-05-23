2d0dcb77fcf25f356f38f7d2c1f69c0feb1c91b7
Yurii Yesipov <yesipov47@gmail.com>
2021-05-23 11:22:47 +0300

    Sign in form

---
+++ lib/extensions/string.dart
@@ -0,0 +1,13 @@
+extension EmailValidator on String {
+  bool isValidEmail() {
+    return RegExp(
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+    ).hasMatch(this);
+  }
+}
+
+extension PasswordValidator on String {
+  bool isValidPassword() {
+    return this.length > 5 && this.length < 21;
+  }
+}
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -4,9 +4,20 @@ import 'package:flutter_svg/svg.dart';
 import 'package:pdoc/constants.dart';
 import 'package:pdoc/l10n/l10n.dart';
 import 'package:pdoc/screens/tabs_screen.dart';
+import 'package:pdoc/extensions/string.dart';
 
 class SignInScreen extends StatelessWidget {
   static const routeName = '/sign-in';
+
+  final _formKey = GlobalKey<FormState>();
+
+  void _onSubmit(BuildContext ctx) {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    Navigator.of(ctx).pushReplacementNamed(TabsScreen.routeName);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,58 +32,72 @@ class SignInScreen extends StatelessWidget {
           child: SafeArea(
             child: Padding(
               padding: EdgeInsets.all(ThemeConstants.spacing(1)),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: ThemeConstants.spacing(1),
-                  ),
-                  SvgPicture.asset(
-                    'assets/images/circle-paw.svg',
-                    color: Theme.of(context).accentColor,
-                  ),
-                  SizedBox(
-                    height: ThemeConstants.spacing(2),
-                  ),
-                  Text(
-                    L10n.of(context).sign_in_screen_welcome_text,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  SizedBox(
-                    height: ThemeConstants.spacing(1),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText:
-                          L10n.of(context).sign_in_screen_email_text_field_text,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: ThemeConstants.spacing(1),
                     ),
-                  ),
-                  SizedBox(
-                    height: ThemeConstants.spacing(1),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: L10n.of(context)
-                          .sign_in_screen_password_text_field_text,
+                    SvgPicture.asset(
+                      'assets/images/circle-paw.svg',
+                      color: Theme.of(context).accentColor,
                     ),
-                  ),
-                  SizedBox(
-                    height: ThemeConstants.spacing(1),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context)
-                            .pushReplacementNamed(TabsScreen.routeName);
-                      },
-                      child: Text(
-                        L10n.of(context).sign_in_screen_sign_in_button_text,
+                    SizedBox(
+                      height: ThemeConstants.spacing(2),
+                    ),
+                    Text(
+                      L10n.of(context).sign_in_screen_welcome_text,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(
+                      height: ThemeConstants.spacing(1),
+                    ),
+                    TextFormField(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      validator: (input) => input!.isValidEmail()
+                          ? null
+                          : L10n.of(context).sign_in_screen_invalid_email_text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: L10n.of(context)
+                            .sign_in_screen_email_text_field_text,
                       ),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: ThemeConstants.spacing(1),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      keyboardType: TextInputType.visiblePassword,
+                      textInputAction: TextInputAction.done,
+                      validator: (input) => input!.isValidPassword()
+                          ? null
+                          : L10n.of(context)
+                              .sign_in_screen_invalid_password_text,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: L10n.of(context)
+                            .sign_in_screen_password_text_field_text,
+                      ),
+                    ),
+                    SizedBox(
+                      height: ThemeConstants.spacing(1),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () => _onSubmit(context),
+                        child: Text(
+                          L10n.of(context).sign_in_screen_sign_in_button_text,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
