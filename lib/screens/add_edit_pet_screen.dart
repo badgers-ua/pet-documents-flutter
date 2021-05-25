da564603455461acf6e851c03c07fadfc648b85e
Yurii Yesipov <yesipov47@gmail.com>
2021-05-25 14:27:33 +0300

    [WIP] Create pet

--- lib/screens/add_edit_pet_screen.dart
+++ lib/screens/add_edit_pet_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pdoc/constants.dart';
 import 'package:pdoc/l10n/l10n.dart';
 
 class AddEditPetScreen extends StatelessWidget {
@@ -7,11 +8,72 @@ class AddEditPetScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(L10n.of(context).add_edit_pet_screen_app_bar_text)),
+      appBar: AppBar(
+          title: Text(L10n.of(context).add_edit_pet_screen_app_bar_text)),
       body: Scrollbar(
         child: SingleChildScrollView(
-          child: Container(
-            child: Text("Create new one"),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(ThemeConstants.spacing(1)),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Name",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Species",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Breed",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Gender",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Date of birth",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Color",
+                      ),
+                    ),
+                    SizedBox(height: ThemeConstants.spacing(1)),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Description",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
